44f2a30feb56f52ebf981ae4f7c56e615c9f8388
LeeJiYeon <dqk8632@naver.com>
2024-03-11 14:16:59 +0900

    Chore: 백앤드 프로젝트 생성

---
+++ backend/foorrng/src/main/java/com/d205/foorrng/FoorrngApplication.java
@@ -0,0 +1,13 @@
+package com.d205.foorrng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoorrngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoorrngApplication.class, args);
+	}
+
+}
---
+++ backend/foorrng/src/test/java/com/d205/foorrng/FoorrngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d205.foorrng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoorrngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
