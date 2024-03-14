c71daf065f001abef6b7916ac5aad51cf5461a78
박정환 <jeonghw4n@naver.com>
2024-03-14 11:03:08 +0900

    feature: UserRepository 생성

---
+++ backend/foorrng/src/main/java/com/d205/foorrng/User/UserRepository.java
@@ -0,0 +1,9 @@
+package com.d205.foorrng.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
