e9ca06be844981e902e46c050361366088e058ce
LeeJiYeon <dqk8632@gmail.com>
2024-03-14 13:59:39 +0900

    Fix: merge confilct solved

--- backend/foorrng/src/main/java/com/d205/foorrng/foodtruck/entity/FoodtruckLike.java
+++ backend/foorrng/src/main/java/com/d205/foorrng/foodtruck/entity/FoodtruckLike.java
@@ -1,6 +1,7 @@
 package com.d205.foorrng.foodtruck.entity;
 
-import com.d205.foorrng.user.User;
+
+import com.d205.foorrng.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
