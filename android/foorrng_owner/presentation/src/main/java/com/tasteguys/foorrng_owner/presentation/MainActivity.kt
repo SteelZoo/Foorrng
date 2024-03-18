94be2a0b39fb5b9ee3c18f4a0329ebc9b5759633
정철주 <cjfwn96@gmail.com>
2024-03-18 17:38:44 +0900

    feat: 점주앱/ 데이터 통신 관계없는 기본 이동 로직 구현

--- android/foorrng_owner/presentation/src/main/java/com/tasteguys/foorrng_owner/presentation/MainActivity.kt
+++ android/foorrng_owner/presentation/src/main/java/com/tasteguys/foorrng_owner/presentation/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.tasteguys.foorrng_owner.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.tasteguys.foorrng_owner.presentation.R.*
+import com.tasteguys.foorrng_owner.presentation.R.layout
+import com.tasteguys.foorrng_owner.presentation.base.BaseActivity
+import com.tasteguys.foorrng_owner.presentation.busi_number.BusiNumberFragment
+import com.tasteguys.foorrng_owner.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    ActivityMainBinding::inflate
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.layout_main_fragment,BusiNumberFragment())
+            .commit()
     }
 }
