002ec3f104b718a5a2a4d7049904fb01b4f5b7dd
박정환 <jeonghw4n@naver.com>
2024-03-18 10:57:41 +0900

    feature/API/User

--- backend/foorrng/src/main/java/com/d205/foorrng/user/controller/UserController.java
+++ backend/foorrng/src/main/java/com/d205/foorrng/user/controller/UserController.java
@@ -2,14 +2,18 @@ package com.d205.foorrng.user.controller;
 
 
 import com.d205.foorrng.jwt.token.TokenDto;
+import com.d205.foorrng.user.entity.User;
+import com.d205.foorrng.user.repository.UserRepository;
 import com.d205.foorrng.user.repository.UserRole;
 import com.d205.foorrng.user.service.UserSginService;
 import com.d205.foorrng.user.dto.UserDto;
+import com.d205.foorrng.util.SecurityUtil;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Getter @Setter
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
-public class SignController {
+@RequestMapping("/api/user")
+public class UserController {
 
+    private final UserRepository userRepository;
     private final UserSginService userSginService;
 
     @PostMapping("/regist/owner")
@@ -38,6 +43,17 @@ public class SignController {
         TokenDto response = userSginService.sign(userDto, UserRole.valueOf("USER"));
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
+
+    @GetMapping("")
+    public ResponseEntity<Long> searchUserInfo(TokenDto token) {
+
+        User user = userRepository.findByUserUid(Long.parseLong(SecurityUtil.getCurrentUsername().get())).get();
+
+        return ResponseEntity.status(HttpStatus.OK).body(user.getUserUid());
+
+    }
+
 
 
     // jwt 토큰 작동 체크를 위한 api
