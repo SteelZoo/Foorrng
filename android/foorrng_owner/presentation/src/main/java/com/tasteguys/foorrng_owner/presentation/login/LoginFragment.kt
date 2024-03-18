2c2cbbfe5a1f9afcd6cbc8f3d4169e6dffc67572
정철주 <cjfwn96@gmail.com>
2024-03-18 16:00:37 +0900

    feat: 로그인 프래그먼트 뒤로가기 버튼 대응

--- android/foorrng_owner/presentation/src/main/java/com/tasteguys/foorrng_owner/presentation/login/LoginFragment.kt
+++ android/foorrng_owner/presentation/src/main/java/com/tasteguys/foorrng_owner/presentation/login/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.tasteguys.foorrng_owner.presentation.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import com.tasteguys.foorrng_owner.presentation.LoginActivity
 import com.tasteguys.foorrng_owner.presentation.R
 import com.tasteguys.foorrng_owner.presentation.base.BaseFragment
 import com.tasteguys.foorrng_owner.presentation.databinding.FragmentLoginBinding
@@ -12,10 +15,21 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>(
-    FragmentLoginBinding::bind,R.layout.fragment_login
+    FragmentLoginBinding::bind, R.layout.fragment_login
 ) {
+
+    private lateinit var loginActivity: LoginActivity
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loginActivity = _activity as LoginActivity
 
+        loginActivity.onBackPressedDispatcher.addCallback(viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    loginActivity.finish()
+                }
+            }
+        )
     }
 }
