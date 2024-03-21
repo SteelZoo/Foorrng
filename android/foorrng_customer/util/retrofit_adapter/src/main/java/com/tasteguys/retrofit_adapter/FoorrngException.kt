4618273007857a86a1479e7be8070af6123b245b
Kunwoo Kim <rlarjsdn0407@naver.com>
2024-03-21 23:02:07 +0900

    fix - Unversioned File (retrofit util) push

---
+++ android/foorrng_customer/util/retrofit_adapter/src/main/java/com/tasteguys/retrofit_adapter/FoorrngException.kt
@@ -0,0 +1,11 @@
+package com.tasteguys.retrofit_adapter
+
+class FoorrngException(
+    val code: String,
+    override val message: String
+):Exception(message){
+
+    companion object{
+        const val NOT_EXIST_USER = "U-001"
+    }
+}
