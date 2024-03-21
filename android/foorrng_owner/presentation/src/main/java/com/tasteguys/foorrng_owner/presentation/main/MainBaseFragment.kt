b3b3bbbc3e7210ccd42cebb1e761a44e5e2cefe9
정철주 <cjfwn96@gmail.com>
2024-03-21 17:13:47 +0900

    feat: 점주앱/ 홈프래그먼트 생성 및 기본 구성

--- android/foorrng_owner/presentation/src/main/java/com/tasteguys/foorrng_owner/presentation/main/MainBaseFragment.kt
+++ android/foorrng_owner/presentation/src/main/java/com/tasteguys/foorrng_owner/presentation/main/MainBaseFragment.kt
@@ -1,5 +1,6 @@
 package com.tasteguys.foorrng_owner.presentation.main
 
+import android.content.Context
 import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.activityViewModels
@@ -13,9 +14,15 @@ abstract class MainBaseFragment<B : ViewBinding>(
     bind,layoutResId
 ), IToolbarFragment {
     protected val mainViewModel: MainViewModel by activityViewModels()
+    protected lateinit var mainActivity: MainActivity
 
     override fun setToolbar() {
         mainViewModel.changeToolbar(MainToolbarControl())
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
     }
 
     override fun onStart() {
