cd9ce531133b1f2a7598cdc23e31846fb3b75a85
이지연 <dqk8632@gmail.com>
2024-04-01 14:36:26 +0900

    fix: foodtrucks review 일대다 연결

--- backend/foorrng/src/main/java/com/d205/foorrng/foodtruck/entity/Foodtrucks.java
+++ backend/foorrng/src/main/java/com/d205/foorrng/foodtruck/entity/Foodtrucks.java
@@ -1,18 +1,17 @@
 package com.d205.foorrng.foodtruck.entity;
 
 import com.d205.foorrng.food.Food;
-import com.d205.foorrng.foodtruck.response.FoodtruckResDto;
 import com.d205.foorrng.mark.Mark;
 import com.d205.foorrng.requestDelete.RequestDelete;
+import com.d205.foorrng.review.Review;
 import com.d205.foorrng.user.entity.User;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-
-import static com.d205.foorrng.user.repository.UserRole.OWNER;
 
 @Entity
 @Getter
@@ -58,6 +57,10 @@ public class Foodtrucks {
 
     @OneToMany(mappedBy = "foodtrucks", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<FoodtruckReport> foodtruckReportList;
+
+    @OneToMany(mappedBy = "foodtrucks", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Review> reviews;
+
 
     @Builder
     public Foodtrucks(FoodtruckRole foodtruckRole, User user){
